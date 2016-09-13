c1cdf5bd8a8942698c63ed66e99610428a0f51e4
tacksoo <tacksoo.im@gmail.com>
2016-09-13 03:26:56 +0000

    started simple excel file upload and read in flask using openpyxl

--- flask_app.py
+++ flask_app.py
@@ -1,17 +1,42 @@
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
-from flask import request
-from flask import jsonify
+# Started based on this sample:
+# http://stackoverflow.com/questions/27628053/uploading-and-downloading-files-with-flask
+from flask import Flask, make_response, request
+import openpyxl
 
 app = Flask(__name__)
 
+
+def findColumnByName(sheet,name):
+  for c in range(ord('A'),ord('Z')+1):
+    if sheet.cell( chr(c) + '1' ).value == name:
+      return chr(c)
+
 @app.route('/')
-def hello_world():
-  #jsonify({'ip': request.remote_addr}), 200
-  return "Hello World"
+def form():
+    return """
+        <html>
+            <body>
+                <h1>Do something with Excel Spreadsheet</h1>
 
-@app.route('/dude')
-def dude():
-  return "<h1>Dude!</h1>"
+                <form action="/transform" method="post" enctype="multipart/form-data">
+                    <input type="file" name="data_file" />
+                    <input type="submit" />
+                </form>
+            </body>
+        </html>
+    """
 
+@app.route('/transform', methods=["POST"])
+def transform_view():
+    file = request.files['data_file']
+    if not file:
+        return "No file"
+
+    # openpyxl documentation found here: https://media.readthedocs.org/pdf/openpyxl/latest/openpyxl.pdf
+    wb = openpyxl.load_workbook(file)
+    sheet = wb.get_sheet_by_name('MAIN')
+    column = findColumnByName(sheet,'INSTRUCTOR')
+
+    response = make_response(str(column))
+    response.headers["Content-Disposition"] = "attachment; filename=result.csv"
+    return response
